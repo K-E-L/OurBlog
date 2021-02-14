c7398d85eb50c73a71ce8d512b6804a1e8d02231
Dillon Ching <dk8ching@gmail.com>
2021-02-13 19:10:34 -0800

    init

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Welcome
+Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/dashboard', [WelcomeController::class, 'dashboard'])
+    ->middleware(['auth'])->name('dashboard');
+
+// Post
+Route::post('/posts/create', [PostController::class, 'create']);
+Route::post('/posts/{post}/destroy', [PostController::class, 'destroy']);
+Route::post('/posts/{post}/edit', [PostController::class, 'edit']);
+
+
+
+
+require __DIR__.'/auth.php';
